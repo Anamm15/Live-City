3d3c8e093adbe50f3df88b017386c0dbe8684ed5
Choirul Anam <choirulanm.15@gmail.com>
2025-07-31 19:39:16 +0700

    feat: testing api

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import authRoutes from './auth.route';
 import submissionRoutes from './submission.route';
 import reportRoutes from './report.route';
 import newsRoutes from './news.route';
+import familyRoutes from './family.route';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/users', userRoutes);
 router.use('/submissions', submissionRoutes)
 router.use('/reports', reportRoutes)
 router.use('/news', newsRoutes)
+router.use('/families', familyRoutes)
 
 export default router;
